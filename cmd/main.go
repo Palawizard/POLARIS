c5d6c9e77ae15b1fa2902fba493abc45846d8d41
Palawi <bapcoloj@gmail.com>
2025-09-09 18:18:45 +0200

    création de initgame() dans game.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"projet-red_POLARIS/internal/character"
-	"projet-red_POLARIS/internal/menu"
-)
+import game "projet-red_POLARIS/internal"
 
 func main() {
-	c1 := character.InitCharacter("Palawi", "Elfe", 1, 100, 40, map[string]int{"Potion": 3})
-	menu.ShowMenu(c1)
+	game.InitGame()
 }
--- internal/game.go
+++ internal/game.go
@@ -1,1 +1,11 @@
 package game
+
+import (
+	"projet-red_POLARIS/internal/character"
+	"projet-red_POLARIS/internal/menu"
+)
+
+func InitGame() {
+	c1 := character.InitCharacter("Palawi", "Elfe", 1, 100, 40, map[string]int{"Potion": 3})
+	menu.ShowMenu(c1)
+}
