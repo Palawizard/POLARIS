8bf43d62bb5b13be17c6daf1250401403c91b7ad
dejules <jules.demetzizard@ynon.com>
2025-09-11 11:19:08 +0200

    changeprice

--- internal/shop/shop.go
+++ internal/shop/shop.go
@@ -13,35 +13,41 @@ func Shop(player *utils.Player) {
 	fmt.Println("Shop")
 	fmt.Print("\n\n")
 	fmt.Println("=== Bienvenue chez le marchand ===")
-	fmt.Println("1. Potion de vie (GRATUITE)")
-	fmt.Println("2. Potion de poison (10$)")
-	fmt.Println("3. Livre de Sort: Boule de feu (70$)")
+	fmt.Println("1. Potion de vie (3$)")
+	fmt.Println("2. Potion de poison (6$)")
+	fmt.Println("3. Livre de Sort: Boule de feu (25$)")
 	fmt.Println("4. Retour")
 
 	var choice int
 	fmt.Scan(&choice)
 	switch choice {
 	case 1:
+		if player.Money < 3 {
+			fmt.Println("Vous n'avez pas assez d'argent.")
+			time.Sleep(2 * time.Second)
+			return
+		}
+		player.Money -= 3
 		character.AddInventory(player, "Potion")
 		fmt.Println("Vous recevez 1 Potion de heal. Total :", player.Inventory["Potion"])
 		time.Sleep(2 * time.Second)
 	case 2:
-		if player.Money < 10 {
+		if player.Money < 6 {
 			fmt.Println("Vous n'avez pas assez d'argent.")
 			time.Sleep(2 * time.Second)
 			return
 		}
-		player.Money -= 10
+		player.Money -= 6
 		character.AddInventory(player, "Poison")
 		fmt.Println("Vous recevez 1 Potion de poison. Total :", player.Inventory["Poison"])
 		time.Sleep(2 * time.Second)
 	case 3:
-		if player.Money < 70 {
+		if player.Money < 25 {
 			fmt.Println("Vous n'avez pas assez d'argent.")
 			time.Sleep(2 * time.Second)
 			return
 		}
-		player.Money -= 70
+		player.Money -= 25
 		skills.SpellBook("Boule de feu", player)
 		fmt.Println("Vous recevez 1 Livre de Sort: Boule de feu. Total :", player.Inventory["Boule de feu"])
 		time.Sleep(2 * time.Second)
