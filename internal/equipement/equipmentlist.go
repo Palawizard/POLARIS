c31922928939c6b49c816c09cf809d2cca70fd18
baptiste <baptiste.demenorval@ynov.com>
2025-09-11 15:44:44 +0200

    shopfix, itemfix and comments

--- internal/equipement/equipmentlist.go
+++ internal/equipement/equipmentlist.go
@@ -10,36 +10,7 @@ type Equipment struct {
 	Defense int
 }
 
-var Equipments = map[string]Equipment{
-	"Wolf Fur": {
-		ID:      "Wolf Fur",
-		Name:    "Wolf Fur",
-		Type:    "Fur",
-		Price:   4,
-		Defense: 0,
-	},
-	"Troll Skin": {
-		ID:      "Troll Skin",
-		Name:    "Troll Skin",
-		Type:    "Skin",
-		Price:   7,
-		Defense: 0,
-	},
-	"Boar Leather": {
-		ID:      "Boar Leather",
-		Name:    "Boar Leather",
-		Type:    "Leather",
-		Price:   3,
-		Defense: 0,
-	},
-	"Crow Feather": {
-		ID:      "Crow Feather",
-		Name:    "Crow Feather",
-		Type:    "Feather",
-		Price:   1,
-		Defense: 0,
-	},
-}
+var Equipments = map[string]Equipment{}
 
 // GetEquipment returns an Equipment from the Equipments map.
 // If the Equipment doesn't exist, it returns an empty Equipment.
--- internal/shop/spellshop.go
+++ internal/shop/spellshop.go
@@ -8,29 +8,37 @@ import (
 	"time"
 )
 
-// Spellshop is a menu allowing the player to buy a spellbook. It will
-// display the player's current coins, and then display a list of spells
-// available for purchase. The player is prompted to enter the number of the
-// spell they wish to purchase. If the player enters a number that is not
-// in the range of the options, or if they do not have enough coins, it will
-// print out an error message and then loop back to the start of the menu.
-// If the player chooses to purchase a spell, it will be added to their
-// spellbook and the cost will be deducted from their coins. After the
-// spell is purchased, the player will be prompted to enter "1" to return.
+// Spellshop displays the player's spellbook and allows them to buy spells.
+// It will display the player's current coins, and then display a list of
+// available spells. The player is prompted to enter the number of the
+// spell they wish to buy. If the player enters a number that is not in the
+// range of the options, or if they do not have enough coins, or if their
+// inventory is full, it will simply loop back to the start of the menu. If
+// the player chooses to buy a spell, it will be added to their spellbook,
+// the cost of the spell will be subtracted from their money, and the player
+// will be prompted to enter "1" to return.
 func Spellshop(player *utils.Player) {
 	lastMsg := ""
+	catalog := []string{"Fire Ball"}
+	prices := map[string]int{"Fire Ball": 25}
+
 	for {
 		utils.Clearscreen()
 		fmt.Println("<=== Spell Shop ===>")
 		fmt.Printf("Coins: %d\n\n", player.Money)
 
-		ownedFire := 0
-		if player.Skills != nil {
-			ownedFire = player.Skills["Fire Ball"]
+		for i, id := range catalog {
+			label := id
+			if s, ok := skills.Skills[id]; ok && s.Label != "" {
+				label = s.Label
+			}
+			owned := 0
+			if player.Skills != nil {
+				owned = player.Skills[id]
+			}
+			fmt.Printf("%d. Spellbook: %s (%d coins)  [owned: x%d]\n", i+1, label, prices[id], owned)
 		}
-
-		fmt.Printf("1. Spellbook: Fire Ball (25$)  [owned: x%d]\n", ownedFire)
-		fmt.Println("2. Return")
+		fmt.Printf("%d. Return\n", len(catalog)+1)
 
 		if lastMsg != "" {
 			fmt.Println()
@@ -39,27 +47,40 @@ func Spellshop(player *utils.Player) {
 
 		var choice int
 		fmt.Scan(&choice)
-		switch choice {
-		case 1:
-			if !character.CheckInvSize(player) {
-				lastMsg = "Your inventory is full."
-				time.Sleep(1 * time.Second)
-				continue
-			}
-			if player.Money < 25 {
-				lastMsg = "You do not have enough coins."
-				time.Sleep(1 * time.Second)
-				continue
-			}
-			player.Money -= 25
-			if skills.SpellBook("Fire Ball", player) {
-				lastMsg = fmt.Sprintf("You received 1 Spellbook: Fire Ball, total : %d", player.Skills["Fire Ball"])
-			} else {
-				lastMsg = "Can't buy that."
-			}
-			time.Sleep(1 * time.Second)
-		case 2:
+
+		if choice == len(catalog)+1 {
 			return
 		}
+		if choice < 1 || choice > len(catalog) {
+			lastMsg = "Invalid choice."
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		id := catalog[choice-1]
+		price := prices[id]
+		label := id
+		if s, ok := skills.Skills[id]; ok && s.Label != "" {
+			label = s.Label
+		}
+
+		if !character.CheckInvSize(player) {
+			lastMsg = "Your inventory is full."
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		if player.Money < price {
+			lastMsg = "You do not have enough coins."
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		player.Money -= price
+		if skills.SpellBook(id, player) {
+			lastMsg = fmt.Sprintf("You received 1 Spellbook: %s, total : %d", label, player.Skills[id])
+		} else {
+			lastMsg = "Can't buy that."
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
